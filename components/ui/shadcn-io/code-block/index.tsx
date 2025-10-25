1dfd960cb963ef77c15f9cf3bbb440373c7a90d3
macxnet040 <lars.macario@gmail.com>
2025-10-25 18:10:18 +0200

    Remove TypeScript error suppression for button click event in CodeBlockCopyButton component to enhance type safety and code clarity.

--- components/ui/shadcn-io/code-block/index.tsx
+++ components/ui/shadcn-io/code-block/index.tsx
@@ -466,7 +466,6 @@ export const CodeBlockCopyButton = ({
 
   if (asChild) {
     return cloneElement(children as ReactElement, {
-      // @ts-expect-error - we know this is a button
       onClick: copyToClipboard,
     });
   }
